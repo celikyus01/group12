be8f1926ab41d1cba6a25ababdf10d1eda8dd968
celikyus01 <62037357+celikyus01@users.noreply.github.com>
2022-04-23 11:00:38 +0100

    cukes runner added

---
+++ src/test/java/com/cydeo/group12/runners/Any.java
@@ -0,0 +1,5 @@
+package com.cydeo.group12.runners;
+
+public class Any {
+}
+
---
+++ src/test/java/com/cydeo/group12/runners/CukesRunner.java
@@ -0,0 +1,10 @@
+package com.cydeo.group12.runners;
+
+import io.cucumber.junit.CucumberOptions;
+
+public class CukesRunner {
+
+
+
+    @CucumberOptions
+}
